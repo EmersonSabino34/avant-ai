9fd2be9e26b2608b831643dc19fe57fb1dda047b
emerson sabino <emersonsabino34@gmail.com>
2025-02-22 19:00:34 +0000

    updated

--- src/app/forgot/page.tsx
+++ src/app/forgot/page.tsx
@@ -1,6 +1,29 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Forgot() {
+
+  /** Controle de Estados
+   *  input -> email  
+   *  buttton -> redefinir
+   *  container -> bg-container
+   *  textEmail ->
+   * 
+   *  Funções
+   */
+  
+  const [email, setEmail] = useState("")
+  const [textEmail, setTextEmail] = useState("")
+  const [btnReset, setBtnReset] = useState("bg-blue-700")
+  const [container, setContainer] = useState("bg-slate-100")
+
+  function handleReset(e: any){
+      e.preventDefault()
+
+      
+  }
+
   return (
     <div className="bg-slate-100 w-screen h-screen flex items-center justify-center">
       <div className="w-1/5 h-1/2 flex flex-col items-center gap-4">
@@ -18,12 +41,14 @@ export default function Forgot() {
 
           <form className=" flex flex-col w-80 m-4 gap-1">
             <label htmlFor="email" className="text-sm font-semibold">E-mail</label>
-            <input type="email" name="email" className="w-80 border-2 p-2 rounded-md" />
+            <input 
+               onChange={event => setEmail( event.target.value)} value={email}
+                type="email" name="email" className="w-80 border-2 p-2 rounded-md" />
+            <p className={`text-red-500 ${textEmail}`}>por favor preencher o campo email</p>
           </form>
 
-          <Link href="/">
-            <button className="bg-blue-700 w-80 mb-3 p-2 text-white text-sm rounded-md font-semibold cursor-pointer">Redefinir senha</button>
-          </Link>
+            <button onClick={handleReset} className={` ${setBtnReset} w-80 mb-3 p-2 text-white text-sm rounded-md font-semibold cursor-pointer`}>Redefinir senha</button>
+         
 
         </div>
       </div>
