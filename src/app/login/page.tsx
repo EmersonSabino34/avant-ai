a39e9ff388f6253dbf72740f1776c50f21464d5e
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-09-25 23:11:25 +0100

    Atualizar o page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,6 @@ export default function Login() {
   const [textEmail, setTextEmail] = useState("hidden")
   const [textPassword, setTextPassword] = useState("hidden")
 
-
   const [btnEnter, setBtnEnter] = useState("bg-blue-700")
   const [container, setContainer] = useState("bg-slate-100")
 
